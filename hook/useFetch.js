702662ba905f16b295cc61c21a41d5de2b8fc575
Raj Chaudhary <craj68168@gmail.com>
2024-04-02 20:44:32 +0545

    feat:api endpoint updated

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,16 +1,17 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-
+import { RAPID_API_KEY } from "@env";
 const useFetch = (endpoint, query) => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  const rapidApiKey = RAPID_API_KEY;
 
   const options = {
     method: "GET",
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     headers: {
-      "X-RapidAPI-Key": "",
+      "X-RapidAPI-Key": rapidApiKey,
       "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
     },
     params: { ...query },
@@ -18,10 +19,8 @@ const useFetch = (endpoint, query) => {
 
   const fetchData = async () => {
     setIsLoading(true);
-
     try {
       const response = await axios.request(options);
-
       setData(response.data.data);
       setIsLoading(false);
     } catch (error) {
